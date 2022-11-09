397192326efe589ee25c02100a0efb3cfb257382
Akshay Soni <109035961+akshaysoni10@users.noreply.github.com>
2022-11-09 21:03:49 +0530

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect("mongodb+srv://akshay-soni:BwSCSbNSioh3vTl1@cluster0.aavafvk.mongodb.net/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch(console.error);
+
+const Todo = require('./models/Todo')
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+});
+app.post('/todo/new', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+    todo.save();
+    res.json(todo);
+});
+
+app.delete('/todo/delete/:id', async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+  res.json({result});
+})
+
+app.get('/todo/complete/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
+
+	todo.complete = !todo.complete;
+
+	todo.save();
+
+	res.json(todo);
+})
+
+
+app.put('todo/update/:id', async(req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  todo.text = req.body.text;
+  todo.save();
+  res.json(todo);
+})
+app.listen(3001);
